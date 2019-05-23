d8bff777b6503822d761163a51cd5beef0074b1a
Belema Ogan <Oganbelema@gmail.com>
2019-05-23 17:17:21 +0100

    Create Network Call to fetch weather data
    - Create Uri for network query
    - Create URL object for network query
    - Create Async class to handle network process in the background
    - Add internet permission

--- S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -15,9 +15,16 @@
  */
 package com.example.android.sunshine;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+
+import com.example.android.sunshine.data.SunshinePreferences;
+import com.example.android.sunshine.utilities.NetworkUtils;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,13 +41,13 @@ public class MainActivity extends AppCompatActivity {
          */
         mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
 
-        // TODO (4) Delete the dummy weather data. You will be getting REAL data from the Internet in this lesson.
+        //  (4) Delete the dummy weather data. You will be getting REAL data from the Internet in this lesson.
         /*
          * This String array contains dummy weather data. Later in the course, we're going to get
          * real weather data. For now, we want to get something on the screen as quickly as
          * possible, so we'll display this dummy data.
          */
-        String[] dummyWeatherData = {
+        /*String[] dummyWeatherData = {
                 "Today, May 17 - Clear - 17°C / 15°C",
                 "Tomorrow - Cloudy - 19°C / 15°C",
                 "Thursday - Rainy- 30°C / 11°C",
@@ -55,24 +62,56 @@ public class MainActivity extends AppCompatActivity {
                 "Sat, May 28 - Meteors - 16°C / 7°C",
                 "Sun, May 29 - Apocalypse - 16°C / 8°C",
                 "Mon, May 30 - Post Apocalypse - 15°C / 10°C",
-        };
+        };*/
 
-        // TODO (3) Delete the for loop that populates the TextView with dummy data
+        //  (3) Delete the for loop that populates the TextView with dummy data
         /*
          * Iterate through the array and append the Strings to the TextView. The reason why we add
          * the "\n\n\n" after the String is to give visual separation between each String in the
          * TextView. Later, we'll learn about a better way to display lists of data.
          */
-        for (String dummyWeatherDay : dummyWeatherData) {
+        /*for (String dummyWeatherDay : dummyWeatherData) {
             mWeatherTextView.append(dummyWeatherDay + "\n\n\n");
-        }
+        }*/
 
-        // TODO (9) Call loadWeatherData to perform the network request to get the weather
+        //  (9) Call loadWeatherData to perform the network request to get the weather
+        loadWeatherData();
     }
 
-    // TODO (8) Create a method that will get the user's preferred location and execute your new AsyncTask and call it loadWeatherData
+    //  (8) Create a method that will get the user's preferred location and execute your new AsyncTask and call it loadWeatherData
+    private void loadWeatherData(){
+        String location = SunshinePreferences.getPreferredWeatherLocation(this);
+        URL queryUrl = NetworkUtils.buildUrl(location);
+        new WeatherAsyncCall().execute(queryUrl);
+    }
 
-    // TODO (5) Create a class that extends AsyncTask to perform network requests
-    // TODO (6) Override the doInBackground method to perform your network requests
-    // TODO (7) Override the onPostExecute method to display the results of the network request
+    //  (5) Create a class that extends AsyncTask to perform network requests
+    //  (6) Override the doInBackground method to perform your network requests
+    //  (7) Override the onPostExecute method to display the results of the network request
+
+    class WeatherAsyncCall extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL queryUri = urls[0];
+
+            String jsonResponse = null;
+
+            try {
+                jsonResponse = NetworkUtils.getResponseFromHttpUrl(queryUri);
+            } catch (IOException error){
+                error.printStackTrace();
+            }
+
+            return jsonResponse;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if (s != null && !s.isEmpty()){
+                mWeatherTextView.setText(s);
+            }
+        }
+    }
 }
